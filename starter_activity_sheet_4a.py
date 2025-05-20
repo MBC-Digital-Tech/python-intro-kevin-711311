13914e7af492916477880ec34c403d8186d309e1
kevin-711311 <kb23015@mbc.school.nz>
2025-05-21 10:57:12 +1200

    Finished

--- starter_activity_sheet_4a.py
+++ starter_activity_sheet_4a.py
@@ -7,19 +7,114 @@ from turtle import *
 # this line says we are using the "turtle" library of functions
 # remember to use this for turtle graphics in Python
 
-
-def mystery():
-    color('green', 'red')
+def square(): #will use loops in the next module, for now just following the instructions
+    color('black', 'gray')
     begin_fill()
-    forward (100) 
+    forward (300) 
     right (90)
-    forward (100) 
+    forward (300) 
     right (90) 
-    forward (100) 
+    forward (300)
     right (90) 
-    forward (100)
+    forward (300)
     right (90)
     end_fill()
+
     done()
 
-mystery()
+def triangle():
+    color('purple', 'blue')
+    begin_fill()
+    
+    for counter in range(3):
+        right(120)
+        forward(200)
+
+    end_fill()
+    
+    done()
+
+def triangle_and_square():
+    color('purple', 'blue')
+    begin_fill()
+    
+    for counter in range(3):
+        right(120)
+        forward(200)
+
+    end_fill()
+
+    penup()
+    forward(50)
+    pendown()
+
+    color('black', 'gray')
+    begin_fill()
+    forward (300) 
+    right (90)
+    forward (300) 
+    right (90) 
+    forward (300)
+    right (90) 
+    forward (300)
+    right (90)
+    end_fill()
+
+    done()
+
+def earth():
+    color('green', 'yellow')
+    begin_fill()
+    circle(100)
+    end_fill()
+    
+    left(90)
+    penup()
+    forward(25)
+    right(90)
+    pendown()
+    
+    color('orange')
+    begin_fill()
+    circle(75)
+    end_fill()
+
+    left(90)
+    penup()
+    forward(50)
+    right(90)
+    pendown()
+
+    color('red')
+    begin_fill()
+    circle(25)
+    end_fill()
+
+    done()
+
+def triangle_hexagon():
+    
+    for i in range(6):
+        forward(200)
+        right(120)
+        forward(200)
+        right(120)
+        forward(200)
+        right(120)
+        right(60)
+
+    done()
+
+drawing = input("What do you want to draw? (square/triangle/triangle and square/earth/triangle hexagon) ").strip().lower()
+if drawing == "square":
+    square()
+elif drawing == "triangle":
+    triangle()
+elif drawing == "triangle and square":
+    triangle_and_square()
+elif drawing == "earth":
+    earth()
+elif drawing == "triangle hexagon":
+    triangle_hexagon()
+else:
+    print("This input is not on the drawing list!")
